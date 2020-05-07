748e197b98d3d40b7d49f0ae6217382d40f4a0a2
Prabhanshu Aggarwal <prabhanshuaggarwal@gmail.com>
2020-05-07 12:46:45 +0530

    Find the missing element

---
+++ Arrays/Missing_Element.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May  7 12:44:48 2020
+
+@author: Prabhanshu Aggarwal
+"""
+#-------------Solution 1------------------
+def dfinder(arr1, arr2):
+    arr1 = sorted(arr1)
+    arr2 = sorted(arr2)
+    l = len(arr1)
+    print(l)
+    print(arr1)
+    print(arr2)
+    for i in range(0,l):
+        #used for last element
+        if(arr1[i]==arr1[l-1]):
+            print((arr1[i]))
+            break
+            
+        if(arr1[i]!=arr2[i]):
+            print((arr1[i]))
+            break
+        
