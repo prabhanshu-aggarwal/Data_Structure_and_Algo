f2eae2e8ab4fd834be175e430189b3a7f89342ef
Prabhanshu Aggarwal <prabhanshuaggarwal@gmail.com>
2020-05-08 11:32:05 +0530

    def large_cont_sum(arr):
        current_sum = max_sum = arr[0]
    
        for num in arr[1:]:
            current_sum = max(current_sum+num , num)
            max_sum = max(current_sum, max_sum)
    
        return max_sum

---
+++ Arrays/Maximum_cont_sum.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May  8 11:30:38 2020
+
+@author: Prabhanshu Aggarwal
+"""
+
+#Find the maximum continuous sum 
+def large_cont_sum(arr):
+    
+    current_sum = max_sum = arr[0]
+    
+    for num in arr[1:]:
+        #we done this for negative numbers
+        current_sum = max(current_sum+num , num)
+        max_sum = max(current_sum, max_sum)
+        
+    return max_sum
