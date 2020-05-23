08e578697240864af9d47f044948e7e46c9b36cd
Prabhanshu Aggarwal <prabhanshuaggarwal@gmail.com>
2020-05-23 19:54:05 +0530

    Reverse of a String

--- Recursion/Permuation_string.py
+++ Recursion/Permuation_string.py
@@ -5,3 +5,37 @@ Created on Fri May 22 12:00:00 2020
 @author: Prabhanshu Aggarwal
 """
 
+# =============================================================================
+# Fill Out Your Solution Below
+# Let's think about what the steps we need to take here are:
+# 
+# Iterate through the initial string – e.g., ‘abc’.
+# For each character in the initial string, set aside that character and get a list of all permutations of the string that’s left. So, for example, if the current iteration is on 'b', we’d want to find all the permutations of the string 'ac'.
+# 
+# Once you have the list from step 2, add each element from that list to the character from the initial string, and append the result to our list of final results. So if we’re on 'b' and we’ve gotten the list ['ac', 'ca'], we’d add 'b' to those, resulting in 'bac' and 'bca', each of which we’d add to our final results.
+# 
+# Return the list of final results.
+# 
+# Let's go ahead and see this implement
+# 
+# =============================================================================
+
+
+def permute(s):
+    
+    out = []
+    
+    if len(s)==1:
+        out = [s]
+        
+    else:
+        
+        #for each letter in the string 
+        for i, let in enumerate(s):
+                
+                for perm in permute(s[:i]+s[i+1:]):
+                    
+                    out+=[let+perm]
+                    
+    return out
+            
---
+++ Recursion/Reverse_of_string.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 23 19:51:27 2020
+
+@author: Prabhanshu Aggarwal
+"""
+#reverse_list("hello world!")  ==>  !dlrow olleh
+
+
+#Reverse of a String using recursion
+
+def reverse_list(string):
+    if len(string)==0:
+        return 
+    
+    temp=string[0]
+    reverse_list(string[1:])
+    print(temp , end = "")
