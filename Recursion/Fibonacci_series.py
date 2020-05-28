3a193455fcd4550dc29898433e35064c5562a47b
Prabhanshu Aggarwal <prabhanshuaggarwal@gmail.com>
2020-05-28 22:18:39 +0530

    Fibonacci using different methods

--- Recursion/Fibonacci_series.py
+++ Recursion/Fibonacci_series.py
@@ -25,7 +25,7 @@ def fib(n):
     else:
         return fib(n-2)+fib(n-1)
     
-    
+-------------------------------------------------------------------    
 #Example 2:
 #Fibonacci Using looping Technique(Iteration Method)
  
@@ -37,3 +37,42 @@ def fib(n):
         a,b=b,a+b
         
     return a
+	
+#--------------------------------------------------	
+	
+#Example 3: Fib using Memoization 
+fib_cache = {}
+
+def fibdyn(n):
+    #If we have the cached value, Then return it
+    if n in fib_cache:
+        return fib_cache[n]
+    
+    if n==1 or n==0:
+        return n
+    
+    else:
+        value = fib(n-2)+fib(n-1)
+        
+    #Store the cache value and then rturn it.
+    fib_cache[n]=value
+    return value
+    
+for i in range(1,10):
+    print(i , " : " , fibdyn(i))
+	
+	#Method 2:Using LRU cache i.e least least recently used
+
+from functools import lru_cache
+
+@lru_cache()
+def fibdyn(n):
+    
+    if n==1 or n==0:
+        return n
+    
+    else:
+        return fib(n-2)+fib(n-1)
+    
+for i in range(1,5):
+    print(i , " : " , fibdyn(i))
